80ffdaa3b9e06570d32c4c264449fbae87c752a5
Joe Marriott <marriott90@gmail.com>
2017-01-05 10:13:53 +0000

    added instance variables (11)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,34 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike}
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+
+  it { is_expected.to respond_to :release_bike }
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
