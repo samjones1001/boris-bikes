a8edbd065e166264ff53226e1e344176640cf481
Peter Grant-Hay <peter@grant-hay.eu>
2017-01-05 19:45:33 +0100

    added private methods empty & full for SRP(15)

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,17 +8,27 @@ class DockingStation
   end
 
   def dock(bike)
-    fail 'Docking Station is full' if @bikes.count >= 20
+    fail 'Docking Station is full' if full? #@bikes.count >= 20
     @bikes << bike
+
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
-    bike = @bikes.pop
+    fail 'No bikes available' if empty?
+    @bikes.pop
   end
 
   def bike
     @bike
   end
 
+  private
+
+  def full?
+    @bikes.size >= 20
+  end
+
+  def empty?
+    @bikes.size == 0
+  end
 end
