bb0437734130780e0b1751f7eed3240b5f290c96
Joe Marriott <marriott90@gmail.com>
2017-01-05 18:06:50 +0000

    increased dock capacity to 20 (14)

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,14 +3,18 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 
+  def initialize
+    @bikes = []
+  end
+
   def dock(bike)
-    fail 'Docking Station is full' if @bike
-    @bike = bike
+    fail 'Docking Station is full' if @bikes.count >= 20
+    @bikes << bike
   end
 
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    bike = @bikes.pop
   end
 
   def bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,25 +18,13 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-
-  end
-
   describe '#dock' do
     it 'raises an error when the docking station is full' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect { subject.dock(bike) }.to raise_error 'Docking Station is full'
+      20.times { subject.dock Bike.new}
+      expect { subject.dock Bike.new }.to raise_error 'Docking Station is full'
     end
   end
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
 
   it { is_expected.to respond_to(:bike) }
 end
