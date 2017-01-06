ca176dd926299ce5296fe4da7773c7c92ed5ebb0
Sam Jones <sam.jones494@googlemail.com>
2017-01-06 15:14:55 +0000

    adds broken bike errror message

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,7 +3,7 @@ require_relative 'bike'
 class DockingStation
 DEFAULT_CAPACITY = 20
 
-  attr_reader :bike, :capacity
+  attr_reader :capacity
 
   def initialize (capacity = DEFAULT_CAPACITY)
     @bikes = []
@@ -12,26 +12,28 @@ DEFAULT_CAPACITY = 20
 
   def dock(bike)
     fail 'Docking Station is full' if full? #@bikes.count >= 20
-    @bikes << bike
+    bikes << bike
 
   end
 
   def release_bike
     fail 'No bikes available' if empty?
-    @bikes.pop
+    bikes.pop
   end
 
   def bike
-    @bike
+    bike
   end
 
   private
 
+  attr_reader :bike 
+
   def full?
-    @bikes.size >= @capacity
+    bikes.size >= @capacity
   end
 
   def empty?
-    @bikes.size == 0
+    bikes.size == 0
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,6 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'can set a capacity' do
-    #station = DockingStation.new(capacity)
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
   
@@ -28,7 +27,7 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when the docking station is full' do
-      DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new}
+      (subject.capacity).times { subject.dock Bike.new}
       expect { subject.dock Bike.new }.to raise_error 'Docking Station is full'
     end
   end
